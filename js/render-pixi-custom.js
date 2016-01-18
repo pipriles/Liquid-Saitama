8b1a53d415714b38def49dc4d3afd02a8faedc3e
Pipriles <oswald.capriles46@gmail.com>
2016-01-18 18:36:48 -0430

    Water Render

---
+++ js/render-pixi-custom.js
@@ -0,0 +1,5 @@
+function ThresholdFilter()
+{PIXI.filters.AbstractFilter.call(this,null,['precision mediump float;','varying vec2 vTextureCoord;','uniform sampler2D uSampler;','uniform float threshold;','void main(void)','{','    vec4 color = texture2D(uSampler, vTextureCoord);','    vec3 blue = vec3(51.0/255.0, 153.0/255.0, 255.0/255.0);','    if (color.a < threshold) {','       gl_FragColor = vec4(vec3(0.0), 1.0);','    } else {','       gl_FragColor = vec4(blue, 1.0);','    }','}',].join('\n'),{threshold:{type:'1f',value:0.5}});}
+ThresholdFilter.prototype=Object.create(PIXI.filters.AbstractFilter.prototype);ThresholdFilter.prototype.constructor=ThresholdFilter;Object.defineProperties(ThresholdFilter.prototype,{threshold:{get:function()
+{return this.uniforms.threshold.value;},set:function(value)
+{this.uniforms.threshold.value=value;}}});
